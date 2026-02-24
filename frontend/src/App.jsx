d5c6af54da0c1c08579ff9af5a2aed336594f466
Luca Fiore <lf44551@gmail.com>
2026-02-24 11:58:34 +0100

    Aggiungi routing per le pagine principali e rimuovi contenuto di esempio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout.jsx'
+import Home from './pages/Home.jsx'
+import Movies from './pages/Movies.jsx'
+import MovieDetail from './pages/MovieDetail.jsx'
+import NotFound from './pages/NotFound.jsx'
+
 function App() {
   return (
-    <main>
-      <h1>Frontend pronto</h1>
-      <p>Base React + Vite pulita e pronta per lo sviluppo.</p>
-    </main>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:id" element={<MovieDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
