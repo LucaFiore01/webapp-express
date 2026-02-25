3a6ec777ce2e57936247c5c8fbc8ee03c8fd3a1a
Luca Fiore <lf44551@gmail.com>
2026-02-25 13:49:21 +0100

    Aggiungi la logica per gestire le recensioni dei film nel router

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { index, show } = require('../controllers/moviesController');
+const { index, show, storeReview } = require('../controllers/moviesController');
 
 const router = express.Router();
 
 router.get('/', index);
 router.get('/:id', show);
+router.post('/:id/reviews', storeReview);
 
 module.exports = router;
