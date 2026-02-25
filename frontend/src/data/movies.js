ee88b0b2157ad65f7f6f32a903942d7c4c7c995b
Luca Fiore <lf44551@gmail.com>
2026-02-25 13:26:09 +0100

    Aggiungi file movies.js con elenco di film

---
+++ frontend/src/data/movies.js
@@ -0,0 +1,31 @@
+const movies = [
+    {
+        id: 1,
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: 2010,
+        genre: 'Sci-Fi',
+        image: '/movies_cover/inception.jpg',
+        plot: 'Un ladro specializzato nell\'estrazione di segreti dai sogni riceve un incarico impossibile: innestare un\'idea nella mente di una persona.'
+    },
+    {
+        id: 2,
+        title: 'Interstellar',
+        director: 'Christopher Nolan',
+        year: 2014,
+        genre: 'Sci-Fi',
+        image: '/movies_cover/interstellar.jpg',
+        plot: 'Un gruppo di esploratori attraversa un wormhole nello spazio per cercare un nuovo futuro all\'umanità.'
+    },
+    {
+        id: 3,
+        title: 'The Dark Knight',
+        director: 'Christopher Nolan',
+        year: 2008,
+        genre: 'Action',
+        image: '/movies_cover/the-dark-knight.jpg',
+        plot: 'Batman affronta il caos portato da Joker, un criminale geniale che mette Gotham in ginocchio.'
+    },
+]
+
+export default movies
