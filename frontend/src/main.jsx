44b56f6a5aea8735e4d1335068ffbc195f3c5c79
Luca Fiore <lf44551@gmail.com>
2026-02-27 13:26:07 +0100

    Aggiunge il componente LoaderProvider per gestire il contesto di caricamento nell'applicazione

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
+import { LoaderProvider } from './contexts/LoaderContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <LoaderProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LoaderProvider>
   </StrictMode>,
 )
